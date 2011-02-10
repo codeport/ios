522aefcbb90374dfa93026af2184adf9b705b291
jibeom Jung <jibeomjung@jibeom-Jung-ui-MacBook-Air.local>
2011-02-10 19:00:18 +0900

    Dont ask me anything.

---
+++ peterjung/LightTheCandle/Classes/LightTheCandleAppDelegate.m
@@ -0,0 +1,127 @@
+//
+//  LightTheCandleAppDelegate.m
+//  LightTheCandle
+//
+//  Created by jibeom Jung on 11. 2. 9..
+//  Copyright 2011 lg cns. All rights reserved.
+//
+
+#import "LightTheCandleAppDelegate.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation LightTheCandleAppDelegate
+
+@synthesize window;
+@synthesize onOffSwitch;
+@synthesize candleImageView;
+@synthesize candleStateLabel;
+
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    
+    candleState = NO;
+	NSString *candleOnPath = [[NSBundle mainBundle]pathForResource:@"candle on" ofType:@"jpg"];
+	NSString *candleOffPath = [[NSBundle mainBundle]pathForResource:@"candle off" ofType:@"jpg"];
+	
+	candleOffImage = [[UIImage alloc] initWithContentsOfFile:candleOffPath];
+	candleOnImage = [[UIImage alloc] initWithContentsOfFile:candleOnPath];
+	
+	[candleImageView setImage: candleOffImage];
+	onOffSwitch.on = candleState;
+	candleStateLabel.text = @"Candle is Off. please light on";
+    
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+- (void)dealloc {
+	[candleOnImage release];
+	[candleOffImage release];
+    [window release];
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Event Handle
+-(IBAction)toggleCandle:(id)sender {
+	candleState = !candleState;
+	if(candleState){
+		
+		//한바퀴 회전시킨다.
+		CABasicAnimation* rotationAnimation;
+		rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+		rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0 ];
+		rotationAnimation.duration = 2.0;
+		//rotationAnimation.cumulative = YES;
+		rotationAnimation.repeatCount = 1.0; 
+		rotationAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+		
+		[candleImageView.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+		
+		//그림을 바꾼다.
+		[candleImageView setImage:candleOnImage];
+		onOffSwitch.on = YES;
+		candleStateLabel.text = @"Candle is now On. ";
+	}else {
+		[candleImageView setImage:candleOffImage];
+		onOffSwitch.on = NO;
+		candleStateLabel.text = @"Candle is Off. please light on";
+	}
+
+}
+
+
+@end
