0b6384fcfcc0c110291766f519383e16a0639acb
dongwook lee <i015005@gmail.com>
2011-02-11 01:22:05 +0900

    걸어라 인간아:최초 커밋

---
+++ nephilim/LightTheCandle/Classes/AppDelegate/LightTheCandleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  LightTheCandleAppDelegate.h
+//  LightTheCandle
+//
+//  Created by nephilim on 11. 2. 10..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LightTheCandleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
