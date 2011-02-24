d934e8b0b4a9eccc58632da7a8db899b31e6029e
dongwook lee <i015005@gmail.com>
2011-02-24 23:55:38 +0900

    WalkingMan 프로젝트 구조 변경 시작

---
+++ nephilim/WalkingMan/Classes/AppDelegate/WalkingManAppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  WalkingManAppDelegate.h
+//  WalkingMan
+//
+//  Created by nephilim on 11. 2. 24..
+//  Copyright 2011 YakShavingLocus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WalkingManAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+	
+	IBOutlet UIImageView* humanWalkingImageView;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+-(UIImageView*) humanWalkingImageView;
+-(void) setHumanWalkingImageView:(UIImageView*)imageView;
+
+#pragma mark -
+#pragma mark 이벤트 처리 
+
+- (IBAction) nextWalkingFrame:(id)sender;
+
+@end
+
