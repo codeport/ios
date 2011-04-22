a7450ebfb48d297a1647fea8166a2fc0953f2f8e
dongwook lee <i015005@gmail.com>
2011-04-23 01:25:54 +0900

    tbfz: 최초커밋

---
+++ nephilim/TabBarControllerFromZero/Classes/AppDelegate/TabBarControllerFromZeroAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TabBarControllerFromZeroAppDelegate.h
+//  TabBarControllerFromZero
+//
+//  Created by nephilim on 11. 4. 23..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarControllerFromZeroAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
