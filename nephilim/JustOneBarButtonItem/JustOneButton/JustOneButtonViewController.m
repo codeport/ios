8e04c3bc9841ff9a2538e36685cc23b65e9b1113
nephilim <i015005@gmail.com>
2011-10-11 00:33:22 +0900

    Event Model 2: 버튼 바인딩 및 이벤트 핸들러 추가

--- nephilim/JustOneBarButtonItem/JustOneButton/JustOneButtonViewController.m
+++ nephilim/JustOneBarButtonItem/JustOneButton/JustOneButtonViewController.m
@@ -9,6 +9,7 @@
 #import "JustOneButtonViewController.h"
 
 @implementation JustOneButtonViewController
+@synthesize btnClick;
 
 - (void)didReceiveMemoryWarning
 {
@@ -20,19 +21,32 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //TODO: Bar Button Item의 경우에는?
+    
 }
-*/
+
 
 - (void)viewDidUnload
 {
+    [self setBtnClick:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
 
+- (void)dealloc {
+    [btnClick release];
+    [super dealloc];
+}
+
+#pragma mark - 이벤트 핸들링
+
+-(void) printLog {
+    NSLog(@"로그를 출력합니다.");
+}
 @end
