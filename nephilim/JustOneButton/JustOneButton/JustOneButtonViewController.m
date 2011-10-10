b25439536faa34c585ed0a11b965e1433a5cb1d1
nephilim <i015005@gmail.com>
2011-10-10 23:38:28 +0900

    Event Model 1: 초기 프로젝트

---
+++ nephilim/JustOneButton/JustOneButton/JustOneButtonViewController.m
@@ -0,0 +1,40 @@
+//
+//  JustOneButtonViewController.m
+//  JustOneButton
+//
+//  Created by Dongwook Lee on 11. 10. 8..
+//  Copyright 2011년 i015005@gmail.com. All rights reserved.
+//
+
+#import "JustOneButtonViewController.h"
+
+@implementation JustOneButtonViewController
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+#pragma mark - Event Handling
+
+@end
